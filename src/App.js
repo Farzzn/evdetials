f8e9afd179580d9d144f4caae56ed6efcc4abfb8
Farhan Muhamed Jaffar <90022626+Farzzn@users.noreply.github.com>
2023-07-22 15:49:18 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import VehicleForm from './components/vehicle';
+
+function App() {
+  return (
+    <div>
+      <VehicleForm/>
+    </div>
+  );
+}
+
+export default App;
